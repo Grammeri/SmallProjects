404039e5dc6c50c5e0baefbc42f11997f6016e49
Grammeri <reacttsxgrammer@gmail.com>
2022-03-22 11:37:24 -0500

    First Commit

--- src/components/UseStateComponents.tsx
+++ src/components/UseStateComponents.tsx
@@ -1,47 +1,38 @@
-import React, {useState} from 'react';
+import React, {useState} from "react";
 
 export const UseStateComponents = () => {
-    const [count, setCount] = useState(0)
-    const [message, setMessage] =useState("You are not even close")
+    let [count, setCount]=useState("0")
 
-    const changeCount = (operation:any) =>{
-        if (operation === "add"){
-            if(count<10) setCount(count +1)
-            if (count >5 && count <=9) setMessage("You are getting closer")
-        } else {
-            if (count>0) setCount(count-1)
-        }
-    }
+    let renderBar = () =>{
+const bars = [];
+        for(let i=0; i < count; i++) {
 
-    const renderBar = () => {
-        const bars = [];
-        for (let i = 0; i < count; i++) {
-            bars.push(
-                <div style={{
-                    backgroundColor: "green",
-                    height: "100%",
-                    width: "10%",
-                }}>
-                </div>)
         }
-return bars
     }
 
     return (
         <div>
-            <h1>{message}</h1>
+            <h1>Use State</h1>
             <div style={{
-                border: "0.1rem solid rgba(0,0,0,3)",
-                height: "100px",
-                width: "80%",
-                margin: "2rem auto",
-                display: "flex",
+                border: "2px solid green",
+                height:"100px",
+                width:"800px",
+                margin: "10px auto",
+                display: "flex"
             }}>
-                {renderBar()}
+                <div style={{
+                    height: "100px",
+                    width: "100px",
+                    backgroundColor: "red"
+                }}>
+
+
+                </div>
+
             </div>
-            <button onClick={()=>{changeCount("subtract")}}>Subtract Bar</button>
-            <button onClick={()=>{changeCount ("add")}}>Add Bar</button>
         </div>
-    );
-};
+
+    )
+}
+
 
